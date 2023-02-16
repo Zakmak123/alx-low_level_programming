6c534686ccf27ca42ef7999c0e424ff94cd7cbbf
zakmak123 <zachariakmary@gmail.com>
2023-02-16 09:21:25 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,9 @@
  * main - this is the main function
  * Return: always 0
  */ 
-int main(void){ puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+
+{ puts("\"Programming is like building a multilingual puzzle");
+	
 	return (0);
 }
