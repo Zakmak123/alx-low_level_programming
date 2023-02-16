16782623bea05c674f8274c2ce6f1315ace6ab0b
zakmak123 <zachariakmary@gmail.com>
2023-02-16 08:54:50 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,6 @@
  * Return: always 0
  */ 
 
-int main(void)
-{        puts("\"Programming is like building a multilingual puzzle");
+int main(void){        puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
